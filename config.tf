fdee262e5a2e1af1415c47b138d1570ab907b84d
Dave Arnold <darnold@hashicorp.com>
2018-09-27 13:52:19 -0700

    chaining workspaces together through implicit environment variables

--- config.tf
+++ config.tf
@@ -6,7 +6,8 @@ variable "ATLAS_WORKSPACE_SLUG" {
 
 locals {
     tfe_org = "${element(split("/", var.ATLAS_WORKSPACE_SLUG), 0)}"
-    tfe_workspace = "${element(split("/", var.ATLAS_WORKSPACE_SLUG), 0)}"
+    tfe_workspace = "${element(split("/", var.ATLAS_WORKSPACE_SLUG), 1)}"
+    service_name = "${element(split("-"), var.ATLAS_WORKSPACE_SLUG, 0)}"
 }
 
 variable "k8s_workspace" {
@@ -14,23 +15,17 @@ variable "k8s_workspace" {
     description="Terraform Enterprise k8s Workspace"
 }
 
-
 data "terraform_remote_state" "k8s" {
     backend = "atlas"
     config {
-        name = "${local/tfe_org}/${var.k8s_workspace}"
+        name = "${local.tfe_org}/${var.k8s_workspace}"
     }
 }
 
 
-variable "build_workspace" {
-    type="string"
-    description="Terraform Enterprise Build Workspace"
-}
-
 data "terraform_remote_state" "build" {
     backend = "atlas"
     config {
-        name = "${local/tfe_org}/${var.build_workspace}"
+        name = "${local.tfe_org}/${local.service_name}-Packer-Build"
     }
 }
