6e00d0691fec4e76e09e25f0777c5b32d444455e
Dave Arnold <darnold@hashicorp.com>
2018-09-27 13:39:21 -0700

    adding service_name to variable check

--- version_check.tf
+++ version_check.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "version_check" {
   backend = "atlas"
   config {
-    name = "${var.k8s_organization}/${var.k8s_workspace}-${var.service_name}-VersionCheck"
+    name = "${local.tfe_org}/${var.k8s_workspace}-${data.terraform_remote_state.build.service_name}-VersionCheck"
   }
 }
