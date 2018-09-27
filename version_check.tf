f09dc04269bd34a2eee64eaa95ded322243ea7ff
Dave Arnold <darnold@hashicorp.com>
2018-09-27 14:07:00 -0700

    fixing

--- version_check.tf
+++ version_check.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "version_check" {
   backend = "atlas"
   config {
-    name = "${local.tfe_org}/${local.service_name}-${var.k8s_workspace}-VersionCheck"
+    name = "${local.tfe_org}/${var.service_name}-${var.k8s_workspace}-VersionCheck"
   }
 }
